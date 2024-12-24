63fb238fea4822859792515986a14fe23a0bfad9
shivamkumar2076 <sk748296@gmail.com>
2024-12-24 13:11:17 +0530

    second change

--- src/main/java/com/shivam/chat/repository/RoomRepository.java
+++ src/main/java/com/shivam/chat/repository/RoomRepository.java
@@ -1,9 +1,11 @@
 package com.shivam.chat.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.shivam.chat.entites.Room;
 
+@Repository
 public interface RoomRepository extends MongoRepository<Room, String>{
 	
 	Room findByRoomId(String roomId);
